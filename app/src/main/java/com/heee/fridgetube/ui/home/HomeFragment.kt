9c8ba1a0e95ce30076e8e1feeb8935b775695e68
hee <tkdhi0107@gmail.com>
2021-02-15 17:14:39 +0900

    홈 화면, Youtube 영상 목록 구현(Youtube Player API)

--- app/src/main/java/com/heee/fridgetube/ui/home/HomeFragment.kt
+++ app/src/main/java/com/heee/fridgetube/ui/home/HomeFragment.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.heee.fridgetube.R
 
 class HomeFragment : Fragment() {
@@ -15,17 +16,45 @@ class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val videoIds = arrayOf(
+            "6JYIGclVQdw",
+            "LvetJ9U_tVY",
+            "6JYIGclVQdw",
+            "LvetJ9U_tVY",
+            "6JYIGclVQdw",
+            "LvetJ9U_tVY",
+            "6JYIGclVQdw",
+            "LvetJ9U_tVY",
+            "6JYIGclVQdw",
+            "LvetJ9U_tVY",
+            "6JYIGclVQdw",
+            "LvetJ9U_tVY"
+        )
+
+        val recyclerView: RecyclerView = root.findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.setHasFixedSize(true)  // Let RecyclerView know the layout of RecyclerView has always same size.
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val adapter = VideoListAdapter(videoIds, this.lifecycle)
+        adapter.itemClickListener = object : VideoListAdapter.OnItemClickListener{
+            override fun onItemClicked(position: Int, videoId: String) {
+                Toast.makeText(requireContext(), "$videoId + 클릭", Toast.LENGTH_SHORT).show()
+            }
+        }
+        recyclerView.adapter = adapter
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(requireContext(), "뷰로딩", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/heee/fridgetube/ui/home/VideoListAdapter.kt
@@ -0,0 +1,60 @@
+package com.heee.fridgetube.ui.home
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
+import com.heee.fridgetube.R
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+class VideoListAdapter(private val videoList: Array<String>, private val lifecycle: Lifecycle) :
+    RecyclerView.Adapter<VideoListAdapter.ViewHolder>() {
+
+    var itemClickListener: OnItemClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false)
+
+        val youtubePlayerView: YouTubePlayerView = itemView.findViewById<YouTubePlayerView>(R.id.youtube_player_view)
+        lifecycle.addObserver(youtubePlayerView)
+
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView.setOnClickListener {
+            println("$position 클릭")
+            itemClickListener?.onItemClicked(position, videoList[position])
+        }
+        holder.readyVideo(videoList[position])
+    }
+
+    override fun getItemCount(): Int = videoList.size
+
+    interface OnItemClickListener {
+        fun onItemClicked(position: Int, videoId: String)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var youtubePlayer: YouTubePlayer? = null
+        private var currentVideoId: String? = null
+
+        init {
+            val youtubePlayerView: YouTubePlayerView = itemView.findViewById<YouTubePlayerView>(R.id.youtube_player_view)
+            youtubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+                override fun onReady(initializedYouTubePlayer: YouTubePlayer) { // Called when the player is ready to play video
+                    youtubePlayer = initializedYouTubePlayer
+                    youtubePlayer?.cueVideo(currentVideoId!!, 0F)
+                }
+            })
+        }
+
+        fun readyVideo(videoId: String) {
+            currentVideoId = videoId
+        }
+    }
+
+}
