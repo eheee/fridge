053226332732e5b12cb01de59d79f776e087dc81
hee <tkdhi0107@gmail.com>
2021-03-03 16:09:27 +0900

    냉장고 재료가 포함된 영상을 제공

--- app/src/main/java/com/heee/fridgetube/data/dao/ItemRecipeCrossDao.kt
+++ app/src/main/java/com/heee/fridgetube/data/dao/ItemRecipeCrossDao.kt
@@ -3,6 +3,7 @@ package com.heee.fridgetube.data.dao
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Transaction
+import com.heee.fridgetube.data.Item
 import com.heee.fridgetube.data.ItemWithRecipes
 
 @Dao
@@ -11,4 +12,8 @@ interface ItemRecipeCrossDao {
     @Transaction
     @Query("SELECT * FROM Item")
     suspend fun getItemWithRecipes(): List<ItemWithRecipes>
+
+    @Transaction
+    @Query("SELECT * FROM Item WHERE id IN (:itemIds)")
+    suspend fun getItemWithRecipes(itemIds: List<Long>): List<ItemWithRecipes>
 }
--- app/src/main/java/com/heee/fridgetube/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/heee/fridgetube/ui/home/HomeViewModel.kt
@@ -2,6 +2,7 @@ package com.heee.fridgetube.ui.home
 
 import android.app.Application
 import androidx.lifecycle.*
+import com.heee.fridgetube.data.Item
 import com.heee.fridgetube.data.Recipe
 import com.heee.fridgetube.data.dao.ItemRecipeCrossDao
 import com.heee.fridgetube.data.room.AppDatabase
@@ -17,10 +18,24 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
     private val db: AppDatabase = AppDatabase.getAppDatabase(context)
 
     fun getRecipes() {
+        val cabinetAndItemDao = db.cabinetAndItemDao()
         val itemRecipeCrossDao = db.itemRecipeCrossDao()
         viewModelScope.launch {
-            val itemWithRecipes = itemRecipeCrossDao.getItemWithRecipes()
-            _recipes.value = itemWithRecipes[0].recipes
+            val cabinetAndItems = cabinetAndItemDao.getCabinetAndItem()
+            val itemsInFridge = mutableListOf<Long>()
+            for (cabinetAndItem in cabinetAndItems) {
+                itemsInFridge.add(cabinetAndItem.item.id)
+            }
+
+            val itemWithRecipes = itemRecipeCrossDao.getItemWithRecipes(itemsInFridge)
+            val recipesToShow = mutableListOf<Recipe>()
+            for (itemWithRecipe in itemWithRecipes) {
+                val recipes = itemWithRecipe.recipes
+                for(recipe in recipes){
+                    recipesToShow.add(recipe)
+                }
+            }
+            _recipes.value = recipesToShow
         }
     }
 }
