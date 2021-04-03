077a53df5e004909f5af39d5774520692eb43954
hee <tkdhi0107@gmail.com>
2021-04-03 10:50:24 +0900

    ToolBar 추가

--- app/src/main/java/com/heee/fridgetube/MainActivity.kt
+++ app/src/main/java/com/heee/fridgetube/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.heee.fridgetube.databinding.ActivityMainBinding
+import com.heee.fridgetube.ui.utils.visible
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,9 +25,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-//        val appBarConfiguration = AppBarConfiguration(setOf(
-//                R.id.navigation_home, R.id.navigation_library, R.id.navigation_fridge, R.id.navigation_memo))
-//        setupActionBarWithNavController(navController, appBarConfiguration)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id) {
+                R.id.navigation_detail, R.id.navigation_item ->
+                    binding.navView.visible(false)
+                else -> binding.navView.visible(true)
+            }
+        }
+
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_library, R.id.navigation_fridge, R.id.navigation_memo))
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
         binding.navView.setupWithNavController(navController)
     }
 }
